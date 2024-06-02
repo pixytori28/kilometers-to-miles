41d9f0c546575515a42be713d90b76474b406162
Victoria Inahuazo <victoria.inahuazo11@myhunter.cuny.edu>
2024-06-02 13:10:00 -0400

    Add files via upload

---
+++ h19kmtomiles.py
@@ -0,0 +1,9 @@
+#name:Victoria Inahuazo
+#email:victoria.inahuazo11@myhunter.cuny.edu
+#date: 03/12/24
+#this program converts kilometers to miles
+
+km = float(input('Enter distance in kilometers:'))
+miles = 0.621371 * km
+
+print(miles)
